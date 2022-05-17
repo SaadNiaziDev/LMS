06744ea51ed1eb43dfad99dba073f399151b4cf8
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-05-17 19:48:00 +0500

    Few changes

---
+++ src/components/Login.js
@@ -0,0 +1,86 @@
+import "./style.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+import Dashboard from "./Dashboard"
+import {
+ useNavigate
+} from "react-router-dom";
+
+function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object({
+      email: yup.string().email("Invalid Email Address").required("Required"),
+      password: yup
+        .string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum.")
+        .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+    }),
+    onSubmit: (values) => {
+      axios.post("http://localhost:4000/api/user/login",values).then((response) => {
+        //alert(JSON.stringify(response));
+      }).catch((error) => {
+        alert(error)
+      })
+    },
+  });
+  return (
+    <>
+      <div className="container my-3">
+        <div className="layer">
+          <div className="center">
+            <form onSubmit={formik.handleSubmit}>
+              <div>
+                <p className="header">Login</p>
+                <label htmlFor="Email" className="label">
+                  Email
+                </label>
+                <input
+                  className="input"
+                  type="text"
+                  id="email"
+                  placeholder="Enter your email address"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="warning">Required</div>
+                ): null}
+              </div>
+              <div>
+                <label htmlFor="Password" className="label">
+                  Password
+                </label>
+                <input
+                  className="input"
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="warning">Must be greater than 8 characters</div>
+                ): null}
+              </div>
+              <br></br>
+              <button type="submit">Submit</button>
+
+    
+            </form>
+          
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/components/Signup.js
@@ -0,0 +1,115 @@
+import "./style.css";
+import axios from "axios";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      re_password: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .min(8, "Must be 8 characters or more")
+        .required("Required")
+        .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+      re_password: Yup.string()
+        .min(8, "Must be 8 characters or more")
+        .required("Required")
+        .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      if (values.password === values.re_password) {
+        axios
+          .post("http://localhost:4000/api/user/add", values)
+          .then((response) => {
+              localStorage.setItem("token", response.data.token);
+              alert(JSON.stringify("You hvae been registered! Please Login to continue!"));
+          })
+          .catch(function (error) {
+            alert(JSON.stringify("Mail has been already taken", null, 2));
+          });
+      } else alert(JSON.stringify("Passwords do not match", null, 2));
+    },
+  });
+  return (
+    <>
+      <div className="container my-3">
+        <div className="layer">
+          <div className="center">
+            <form onSubmit={formik.handleSubmit}>
+              <h3 className="header">SignUp</h3>
+
+              <div className="container my-3">
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="warning">Required</div>
+                ) : (
+                  " "
+                )}
+                <label htmlFor="email" className="label">
+                  Email Address
+                </label>
+                <input
+                  className="input"
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+              </div>
+              {formik.touched.password && formik.errors.password ? (
+                <div className="warning">Required</div>
+              ) : null}
+              <div className="container my-3">
+                <label htmlFor="password" className="label">
+                  Password
+                </label>
+                <input
+                  className="input"
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+              </div>
+              <div className="container my-3">
+                {formik.touched.re_password && formik.errors.re_password ? (
+                  <div className="warning">Required</div>
+                ) : (
+                  " "
+                )}
+                <label htmlFor="re_password" className="label">
+                  Re-Password
+                </label>
+                <input
+                  className="input"
+                  id="re_password"
+                  name="re_password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.re_password}
+                />
+              </div>
+
+              <br></br>
+              <button className="button-proceed" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
